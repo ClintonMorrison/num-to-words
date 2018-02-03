4cb9239d066f8bf10a7591592f42225a8987b8c6
Clinton Morrison <contact@clintonmorrison.com>
2018-02-03 13:47:07 -0500

    fix tests and add lint command

--- src/index.js
+++ src/index.js
@@ -1,100 +1,94 @@
 /**
  * This is library provides a method for converting arbitrary integers into english text.
- * For example, it would convert the number "123456" to "one hundred twenty-three thousand four hundred fifty-six"
+ * For example, it would convert the number '123456' to 'one hundred twenty-three thousand four hundred fifty-six'
  *
  * All positive and negative integers are supported. Floating point numbers are rounded to integers
  * before being converted to words.
  *
  *
  * Examples:
- *   numberToWords(0);      // returns "zero"
- *   numberToWords(10001);  // returns "ten thousand one"
- *   numberToWords(111);    // returns "one hundred eleven"
- *   numberToWords(-77);    // returns "negative seventy-seven"
+ *   numberToWords(0);    // returns 'zero'
+ *   numberToWords(10001);  // returns 'ten thousand one'
+ *   numberToWords(111);  // returns 'one hundred eleven'
+ *   numberToWords(-77);  // returns 'negative seventy-seven'
  *
  * @author Clinton Morrison <contact@clintonmorrison.com>
  */
-import { shouldPrefixWithOne, shouldHyphenate } from "./util";
-import { numberList, numberMap } from './numbers';
+import { shouldPrefixWithOne, shouldHyphenate } from './util';
+import { numbers } from './numbers';
 
 /**
  * Converts a number into the corresponding series of english words
  *
  * Examples:
- *    numberToWords(0);      // returns "zero"
- *    numberToWords(10001);  // returns "ten thousand one"
- *    numberToWords(111);    // returns "one hundred eleven"
+ *  numberToWords(0);    // returns 'zero'
+ *  numberToWords(10001);  // returns 'ten thousand one'
+ *  numberToWords(111);  // returns 'one hundred eleven'
  *
  * @param {number} numToConvert
  * @returns {string}
  */
-export function numberToWords(numToConvert) {
-    let i;
-    let n;
-    let words = "";
-    let prefixNum;
-    let remainder;
+export function numberToWords (numToConvert) {
+  let words = '';
+  let prefixNum;
+  let remainder;
+  let closestSmallerNumber;
+  let closestSmallerNumberText;
 
-    // Make sure number is an integer.
-    numToConvert = parseInt(numToConvert, 10);
+  numToConvert = parseInt(numToConvert, 10);
 
-    if (isNaN(numToConvert)) {
-        return "not a number";
+  if (isNaN(numToConvert)) {
+    return 'not a number';
+  }
+
+  if (!isFinite(numToConvert)) {
+    return 'infinity';
+  }
+
+  if (numToConvert < 0) {
+    words += 'negative ';
+    numToConvert *= -1;
+  }
+
+  // Search list of numbers for closest smaller number.
+  // numToConvert will be written in terms of this number.
+  for (const { number, text } of numbers) {
+    if (numToConvert === number) {
+      if (shouldPrefixWithOne(number)) {
+        words += 'one ';
+      }
+      words += text;
+      return words;
     }
 
-    if (!isFinite(numToConvert)) {
-        return "infinity";
+    if (numToConvert > number) {
+      closestSmallerNumber = number;
+      closestSmallerNumberText = text;
+      break;
     }
+  }
 
-    // Prefix negative numbers with 'negative'.
-    if (numToConvert < 0) {
-        words += "negative ";
-        numToConvert *= -1;
-    }
+  // How many 'closestSmallerNumber's can numToConvert be grouped into?
+  // e.g. five 'thousand'.
+  prefixNum = Math.floor(numToConvert / closestSmallerNumber);
+  if (prefixNum !== 1 || shouldPrefixWithOne(closestSmallerNumber)) {
+    words += numberToWords(prefixNum) + ' ';
+  }
 
-    // Search list of numbers for closest smaller number.
-    // numToConvert will be written in terms of this number.
-    for (i = 0; i < numberList.length; i++) {
-        n = numberList[i];
+  words += closestSmallerNumberText;
 
-        // If an exact match is found, just return that word.
-        if (numToConvert === n) {
-            if (shouldPrefixWithOne(n)) {
-                words += "one ";
-            }
-            words += numberMap[n];
-            return words;
-        }
+  remainder = numToConvert - prefixNum * closestSmallerNumber;
+  if (remainder > 0 && shouldHyphenate(closestSmallerNumber)) {
+    words += '-';
+  } else {
+    words += ' ';
+  }
 
-        // A smaller number was found.
-        if (numToConvert > n) {
-            break;
-        }
-    }
-
-    // How many "n"s can numToConvert be grouped into?
-    // e.g. five "thousand".
-    prefixNum = Math.floor(numToConvert / n);
-    if (prefixNum !== 1 || shouldPrefixWithOne(n)) {
-        words += numberToWords(prefixNum) + " ";
-    }
-
-    // Add word for "n".
-    words += numberMap[n];
-
-    // Add hyphen or space to separate parts.
-    remainder = numToConvert - prefixNum * n;
-    if (remainder > 0 && shouldHyphenate(n)) {
-        words += "-";
-    } else {
-        words += " ";
-    }
-
-    // Add word for amount not accounted for yet.
-    if (remainder > 0) {
+  if (remainder > 0) {
     words += numberToWords(remainder);
-    }
+  }
 
-    // Remove trailing whitespace.
-    return words.trim();
+  return words.trim();
 };
+
+export default numberToWords;
--- test/numbersToWord.test.js
+++ test/numbersToWord.test.js
@@ -1,24 +1,34 @@
-import { numbersToWords, numberToWords } from "../src/";
+import { numberToWords } from '../src/';
 
-describe("numbersToWords", function() {
-    const textForNumbers = {
-        1: 'one',
-        10: 'ten',
-        20: 'twenty',
-        21: 'twenty-one',
-        55: 'fifty-five',
-        101: 'one hundred one',
-        1234: 'one thousand two hundred thirty-four',
-        1000000001: 'one billion one',
-        1000000000000025: 'one thousand trillion twenty-five'
+describe('numbersToWords', function () {
+  const textForNumbers = {
+    1: 'one',
+    10: 'ten',
+    20: 'twenty',
+    21: 'twenty-one',
+    55: 'fifty-five',
+    101: 'one hundred one',
+    1234: 'one thousand two hundred thirty-four',
+    1000000001: 'one billion one',
+    1000000000000025: 'one thousand trillion twenty-five'
+  };
 
-    };
-
-    it('outputs the correct text for each number', function () {
-        for (const [number, text] of Object.entries(textForNumbers)) {
-            numberToWords(number).should.equal(text);
-            numberToWords(number * -1).should.equal(`negative ${text}`);
-
-        }
+  for (const [number, text] of Object.entries(textForNumbers)) {
+    it(`returns "${text}" for the number ${number}`, function () {
+      numberToWords(number).should.equal(text);
     });
+
+    it(`returns "negative ${text}" for the number -${number}`, function () {
+      numberToWords(number * -1).should.equal(`negative ${text}`);
+    });
+  };
+
+  it('returns "not a number" when given anything other than a number', function () {
+    numberToWords('test').should.equal('not a number');
+    numberToWords({}).should.equal('not a number');
+    numberToWords(true).should.equal('not a number');
+    numberToWords(false).should.equal('not a number');
+    numberToWords(null).should.equal('not a number');
+    numberToWords(undefined).should.equal('not a number');
+  });
 });
